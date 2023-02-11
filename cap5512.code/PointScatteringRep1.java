ab44f5456d1ac2a8b5798fa811ffc46246eb0a9e
Yousef Abdelsalam <yousefabdelsalam@knights.ucf.edu>
2023-02-11 07:42:22 -0500

    debugged PointScatteringRep1, added PointScatteringRep3

--- cap5512.code/PointScatteringRep1.java
+++ cap5512.code/PointScatteringRep1.java
@@ -1,31 +1,30 @@
 import java.util.*;
 import java.io.*;
 import java.text.*;
-import java.awt.Point;
 
 // X_0, Y_0, X_1, Y_1
 public class PointScatteringRep1 extends FitnessFunction {
-    public PointScattering() throws IOException {
-        name = "Point Scattering Problem";
+    public PointScatteringRep1() throws IOException {
+        name = "Point Scattering Problem Representation #1";
     }
 
-    private double dist(Point p1, Point p2) {
-        return Math.sqrt((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y));
+    private double dist(double[] p1, double[] p2) {
+        return Math.sqrt((p1[0] - p2[0]) * (p1[0] - p2[0]) + (p1[1] - p2[1]) * (p1[1] - p2[1]));
     }
 
     public void doRawFitness(Chromo chromo) {
         double maxDist = -1;
         for (int i = 0; i < Parameters.numGenes; i += 2) {
-            Point p_i = new Point(chromo.getPosIntGeneValue(i) / 1e6, chromo.getPosIntGeneValue(i + 1) / 1e6);
+            double[] p_i = new double[]{chromo.getPosIntGeneValue(i) / 1e6, chromo.getPosIntGeneValue(i + 1) / 1e6};
 
             double minDist = 1e9;
             for (int j = i + 2; j < Parameters.numGenes; j += 2) {
-                Point p_j = new Point(chromo.getPosIntGeneValue(j) / 1e6, chromo.getPosIntGeneValue(j + 1) / 1e6);
+                double[] p_j = new double[]{chromo.getPosIntGeneValue(j) / 1e6, chromo.getPosIntGeneValue(j + 1) / 1e6};
 
                 minDist = Math.min(minDist, dist(p_i, p_j));
             }
 
-            maxDist = max(maxDist, minDist);
+            maxDist = Math.max(maxDist, minDist);
         }
 
         chromo.rawFitness = maxDist;
---
+++ cap5512.code/PointScatteringRep3.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.io.*;
+import java.text.*;
+
+
+// r_0, theta_0, r_1, theta_1, etc...
+public class PointScatteringRep3 extends FitnessFunction {
+    public PointScatteringRep3() throws IOException {
+        name = "Point Scattering Problem Representation #2";
+    }
+
+    private double dist(double r1, int theta1, double r2, int theta2) {
+        // Law of Cosines
+        return Math.sqrt(r1 * r1 + r2 * r2 - 2 * r1 * r2 * Math.cos(theta1 - theta2));
+    }
+
+    public void doRawFitness(Chromo chromo) {
+        double maxDist = -1;
+
+        for (int i = 0; i < Parameters.numGenes; i += 2) {
+            double r_i = chromo.getPosIntGeneValue(i) / 1e6;
+            int theta_i = chromo.getPosIntGeneValue(i + 1) % 360;
+
+            double minDist = 1e9;
+            for (int j = i + 2; j < Parameters.numGenes; j += 2) {
+                double r_j = chromo.getPosIntGeneValue(j) / 1e6;
+                int theta_j = chromo.getPosIntGeneValue(j + 1) % 360;
+
+                minDist = Math.min(minDist, dist(r_i, theta_i, r_j, theta_j));
+            }
+
+            maxDist = Math.max(maxDist, minDist);
+        }
+
+        chromo.rawFitness = maxDist;
+    }
+}
