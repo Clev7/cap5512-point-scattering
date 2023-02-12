32a66eb00d154ee8a960d842e2c71cad46d22096
Yousef Abdelsalam <yousefabdelsalam@knights.ucf.edu>
2023-02-12 18:41:43 -0500

    implemented PointScatteringRep2

--- cap5512.code/PointScatteringRep2.java
+++ cap5512.code/PointScatteringRep2.java
@@ -14,16 +14,15 @@ public class PointScatteringRep2 extends FitnessFunction {
 
     public void doRawFitness(Chromo chromo) {
         double maxDist = -1;
-        for (int i = 0; i < Parameters.numGenes - 2; i += 2) {
-            double[] p_i = new double[]{chromo.getPosIntGeneValue(i) / 1e6, chromo.getPosIntGeneValue(i + (Parameters.numGenes/2)) / 1e6};
+        int n = Parameters.numGenes / 2;
+        for (int i = 0; i < n - 1; i++) {
+            double[] p_i = new double[]{chromo.getPosIntGeneValue(i) / 1e6, chromo.getPosIntGeneValue(i + n) / 1e6};
             p_i[0] = Math.min(p_i[0], 1.0);
             p_i[1] = Math.min(p_i[1], 1.0);
 
             double minDist = 1e9;
-            for (int j = i + 2; j < Parameters.numGenes; j += 2) {
-                double[] p_j = new double[]{chromo.getPosIntGeneValue(j) / 1e6, chromo.getPosIntGeneValue(j + 1) / 1e6};
-
-                // System.out.println("DEBUG: p_i: " + p_i[0] + " " + p_i[1] + " p_j: " + p_j[0] + " " + p_j[1] + " dist: " + dist(p_i, p_j));
+            for (int j = i + 1; j < n; j++) {
+                double[] p_j = new double[]{chromo.getPosIntGeneValue(j) / 1e6, chromo.getPosIntGeneValue(j + n) / 1e6};
 
                 minDist = Math.min(minDist, dist(p_i, p_j));
             }
