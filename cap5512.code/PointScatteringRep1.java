7dfa6dea7b8afaadec02ade0197cc8454b169e27
Yousef Abdelsalam <yousefabdelsalam@knights.ucf.edu>
2023-02-12 19:28:38 -0500

    debugged rep1 and rep2

--- cap5512.code/PointScatteringRep1.java
+++ cap5512.code/PointScatteringRep1.java
@@ -4,6 +4,8 @@ import java.text.*;
 
 // X_0, Y_0, X_1, Y_1
 public class PointScatteringRep1 extends FitnessFunction {
+    private static final int BITS = 21;
+
     public PointScatteringRep1() throws IOException {
         name = "Point Scattering Problem Representation #1";
     }
@@ -12,16 +14,26 @@ public class PointScatteringRep1 extends FitnessFunction {
         return Math.sqrt((p1[0] - p2[0]) * (p1[0] - p2[0]) + (p1[1] - p2[1]) * (p1[1] - p2[1]));
     }
 
+    private void mapToUnitCircle(double[] p) {
+        for (int i = 0; i < p.length; i++) {
+            p[i] -= (1 << (BITS - 1));
+            p[i] /= 1e6;
+
+            p[i] = Math.min(p[i], 1.0);
+            p[i] = Math.max(p[i], -1.0);
+        }
+    }
+
     public void doRawFitness(Chromo chromo) {
         double maxDist = -1;
         for (int i = 0; i < Parameters.numGenes - 2; i += 2) {
-            double[] p_i = new double[]{chromo.getPosIntGeneValue(i) / 1e6, chromo.getPosIntGeneValue(i + 1) / 1e6};
-            p_i[0] = Math.min(p_i[0], 1.0);
-            p_i[1] = Math.min(p_i[1], 1.0);
+            double[] p_i = new double[]{chromo.getPosIntGeneValue(i), chromo.getPosIntGeneValue(i + 1)};
+            mapToUnitCircle(p_i);
 
             double minDist = 1e9;
             for (int j = i + 2; j < Parameters.numGenes; j += 2) {
-                double[] p_j = new double[]{chromo.getPosIntGeneValue(j) / 1e6, chromo.getPosIntGeneValue(j + 1) / 1e6};
+                double[] p_j = new double[]{chromo.getPosIntGeneValue(j), chromo.getPosIntGeneValue(j + 1)};
+                mapToUnitCircle(p_j);
 
                 // System.out.println("DEBUG: p_i: " + p_i[0] + " " + p_i[1] + " p_j: " + p_j[0] + " " + p_j[1] + " dist: " + dist(p_i, p_j));
 
--- cap5512.code/PointScatteringRep2.java
+++ cap5512.code/PointScatteringRep2.java
@@ -4,6 +4,8 @@ import java.text.*;
 
 // X_0, X_1, Y_0, Y_1
 public class PointScatteringRep2 extends FitnessFunction {
+    private static final int BITS = 21;
+
     public PointScatteringRep2() throws IOException {
         name = "Point Scattering Problem Representation #2";
     }
@@ -12,17 +14,27 @@ public class PointScatteringRep2 extends FitnessFunction {
         return Math.sqrt((p1[0] - p2[0]) * (p1[0] - p2[0]) + (p1[1] - p2[1]) * (p1[1] - p2[1]));
     }
 
+    private void mapToUnitCircle(double[] p) {
+        for (int i = 0; i < p.length; i++) {
+            p[i] -= (1 << (BITS - 1));
+            p[i] /= 1e6;
+
+            p[i] = Math.min(p[i], 1.0);
+            p[i] = Math.max(p[i], -1.0);
+        }
+    }
+
     public void doRawFitness(Chromo chromo) {
         double maxDist = -1;
         int n = Parameters.numGenes / 2;
         for (int i = 0; i < n - 1; i++) {
-            double[] p_i = new double[]{chromo.getPosIntGeneValue(i) / 1e6, chromo.getPosIntGeneValue(i + n) / 1e6};
-            p_i[0] = Math.min(p_i[0], 1.0);
-            p_i[1] = Math.min(p_i[1], 1.0);
+            double[] p_i = new double[]{chromo.getPosIntGeneValue(i), chromo.getPosIntGeneValue(i + n)};
+            mapToUnitCircle(p_i);
 
             double minDist = 1e9;
             for (int j = i + 1; j < n; j++) {
-                double[] p_j = new double[]{chromo.getPosIntGeneValue(j) / 1e6, chromo.getPosIntGeneValue(j + n) / 1e6};
+                double[] p_j = new double[]{chromo.getPosIntGeneValue(j), chromo.getPosIntGeneValue(j + n)};
+                mapToUnitCircle(p_j);
 
                 minDist = Math.min(minDist, dist(p_i, p_j));
             }
